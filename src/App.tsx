54320498f3052f40f9566cb5ec319c51d317602a
Gabrielly Barbosa Silva <gabriellybarbosa.dev@gmail.com>
2025-04-03 20:19:48 -0300

    fix: Realizando os testes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Contador from './pages/contador/Contador'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Tarefa from './pages/tarefa/Tarefa'
 
 function App() {
 
   return (
     <>
-        <Home />
+      <Login/>
+      <Contador/>
+      <Tarefa />
     </>
   )
 }
--- src/pages/contador/Contador.tsx
+++ src/pages/contador/Contador.tsx
@@ -1,10 +1,17 @@
-﻿
+﻿import { useState } from "react";
+
 function Contador() {
+
+  const [valor, setValor] = useState(0);
+  function handleClick() {
+    setValor(valor + 1);
+  }
   return (
-
-    const [numero,]
-
-    <div>Contador</div>
+    <div>
+      <h2>Componente Contador</h2>
+      <p>O valor atual é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
   )
 }
 
